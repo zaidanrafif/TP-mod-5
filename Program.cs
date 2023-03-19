dd517b4022c4ce17e9ec2393be7db339aba606e1
zaidanrafif <zaidanajayoo@gmail.com>
2023-03-19 22:09:33 +0700

    menambahkan genericlass dan generic method

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿public class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo user " + user.ToString());
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data.ToString());
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        string nama = "Zaidan";
+        halo.SapaUser(nama);
+
+        DataGeneric<string> data = new DataGeneric<string>("1302213072");
+        data.PrintData();
+    }
+}
